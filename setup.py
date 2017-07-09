262130c6f9f6e833f80a847dc32d050cfac5c3c4
ujiro99 <siro.cola@gmail.com>
2017-07-10 01:41:28 +0900

    Add setup sctipts.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='autologger',
+    version='0.1.0',
+    author='Yujiro Takeda',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['click'],
+)
