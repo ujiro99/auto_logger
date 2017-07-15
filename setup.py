ac78dbadde118f32e1ad789e06f70558b10c2089
ujiro99 <siro.cola@gmail.com>
2017-07-16 02:55:50 +0900

    Add console_scripts setting.

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='autologger',
-    version='0.1.0',
+    version='0.2.0',
     author='Yujiro Takeda',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['click', 'pexpect'],
+    entry_points={
+        "console_scripts": [
+            "plog=logger.cli:main",
+        ],
+    }
 )
