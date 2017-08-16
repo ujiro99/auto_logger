10121a87436f969315e76d7bcaeec10327659dd5
root <siro.cola@gmail.com>
2017-08-16 14:49:47 +0900

    Enables testing local.

--- logger/log.py
+++ logger/log.py
@@ -10,13 +10,32 @@ logger.addHandler(handler)
 logger.propagate = False
 
 def d(msg):
-    logger.debug(msg)
+    """
+    Debug log
+    :param str | bytes msg: Message string.
+    """
+    if isinstance(msg, str):
+        logger.debug(msg)
+    elif isinstance(msg, bytes):
+        logger.debug(msg.decode("utf-8"))
 
 def i(msg):
+    """
+    Info log
+    :param str msg: Message string.
+    """
     logger.info(msg)
 
 def w(msg):
+    """
+    Warning log
+    :param str msg: Message string.
+    """
     logger.warning(msg)
 
 def e(msg):
+    """
+    Error log
+    :param str msg: Message string.
+    """
     logger.error(msg)
