b5c0e97f317004cda12cb39c127b13c9b03480ac
ujiro99 <siro.cola@gmail.com>
2017-08-19 04:03:43 +0900

    version 0.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='autologger',
-    version='0.3.1',
+    version='0.3.2',
     author='Yujiro Takeda',
     packages=find_packages(),
     include_package_data=True,
